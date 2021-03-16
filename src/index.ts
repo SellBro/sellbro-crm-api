981eeffcaa44770f7284029ced0f82e1e651da79
youngrake <youngrake@protonmail.com>
2021-03-16 22:10:55 +0200

    base express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('pepe');
+import 'module-alias/register';
+import 'dotenv/config';
+import express from 'express';
+
+const initExpress = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.listen(process.env.PORT || 8000);
+  console.log(`App started at url: ${process.env.BASE_URL}`);
+};
+
+initExpress();
