601492c6d06972c7750cc1b44e0a2230f1bbdfbc
youngrake <youngrake@protonmail.com>
2021-03-17 14:21:13 +0200

    Error handling

---
+++ src/errors/errorsCustom.ts
@@ -0,0 +1,52 @@
+import { RequestHandler } from 'express';
+
+type ErrorData = { [key: string]: any };
+type ErrorCode = number | string;
+
+export enum Status {
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  INTERNAL_ERROR = 500,
+}
+
+export class CustomError extends Error {
+  constructor(
+    public message: string = '',
+    public code: ErrorCode = 'Internal Server Error',
+    public status: number = Status.INTERNAL_ERROR,
+    public data: ErrorData = {},
+  ) {
+    super();
+  }
+}
+
+export class BadRequestError extends CustomError {
+  constructor(errorData: ErrorData) {
+    super('Errors during validation', 'Bad Request', Status.BAD_REQUEST, errorData);
+  }
+}
+
+export class AuthorizationError extends CustomError {
+  constructor() {
+    super('Authtentication token is invalid', 'Unaithorized', Status.UNAUTHORIZED);
+  }
+}
+
+export class EntityNotFoundError extends CustomError {
+  constructor(entity: string) {
+    super(`Entity '${entity}' not found`, 'Entity Not Found', Status.NOT_FOUND);
+  }
+}
+
+export const catchErrors = (requestHandler: RequestHandler): RequestHandler => async (
+  req,
+  res,
+  next,
+): Promise<any> => {
+  try {
+    return await requestHandler(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+};
